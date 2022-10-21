52a3ad0770710787632dc897a494485c5460a783
Batuhan Ham <batuhan.ham@hotmail.com>
2022-10-21 20:28:59 +0300

    Kdv hesaplayan program tamamlandı.

--- src/KdvHesaplayanProgram/KdvTutari.java
+++ src/KdvHesaplayanProgram/KdvTutari.java
@@ -1,5 +1,27 @@
 package KdvHesaplayanProgram;
 
+import java.util.Scanner;
+
 public class KdvTutari {
-    
+    public static void main(String[] args) {
+
+       double urunFiyat;
+       Scanner klavye = new Scanner(System.in);
+        System.out.print("Ürün fiyatını giriniz : ");
+        urunFiyat = klavye.nextDouble();
+
+        double kdvOran = urunFiyat<1000 ? 0.18 : 0.08;
+        double kdvliFiyat = (urunFiyat + (urunFiyat*kdvOran));
+
+        System.out.println("Ürün fiyatı : " + urunFiyat);
+        System.out.println("Ürün KDV'li fiyatı : " + kdvliFiyat);
+        System.out.println("Uygulanan KDV oranı : " + kdvOran);
+
+
+
+
+
+
+
+    }
 }
