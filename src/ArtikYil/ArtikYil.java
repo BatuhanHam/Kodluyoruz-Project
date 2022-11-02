5e6154172f2baa8c0b3b196c31485049bdb727a7
Batuhan Ham <batuhan.ham@hotmail.com>
2022-11-02 22:43:16 +0300

    Kullanıcıdan alınan yılın artık yıl olup olmadığını hesaplayan program tamamlandı.

---
+++ src/ArtikYil/ArtikYil.java
@@ -0,0 +1,29 @@
+package ArtikYil;
+
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        int yil;
+
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Yılı giriniz :");
+        yil =klavye.nextInt();
+
+        while (yil<0){
+            System.err.println("Yıl negatif olmaz.Tekrar giriniz.");
+            yil = klavye.nextInt();
+        }
+
+        if (yil%100==0 && yil%400 ==0){
+            System.out.println(yil +" bir artık yıldır.");
+        }
+        else if (yil%100 !=0 && yil%4 ==0){
+            System.out.println(yil + " artık bir yıldır.");
+        }
+        else
+            System.out.print(yil + " artık yıl değildir");
+
+
+    }
+}
